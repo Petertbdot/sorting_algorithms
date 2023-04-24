3a57ed65953b380f3e6e6be3297a402513415bb7
Petertbdot <ojooluwatobiloba@gmail.com>
2023-04-24 15:20:00 -0700

    I Added the new Files Solutions

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -5,13 +5,13 @@
  * @a: The first integer to swap.
  * @b: The second integer to swap.
  */
-void swap_ints(int *d, int *f)
+void swap_ints(int *a, int *b)
 {
 	int tmp;
 
-	tmp = *d;
-	*d = *f;
-	*f = tmp;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
 
 /**
